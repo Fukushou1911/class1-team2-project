0e4dd74700ef70543ff36a1dcd667b69d0174872
Fukushou <s1f102401427@iniad.org>
2024-12-07 21:20:55 +0900

    html, css, jsファイルの追加

--- team2app/apps.py
+++ team2app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Team1AppConfig(AppConfig):
+class Team2AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'team1app'
+    name = 'team2app'
