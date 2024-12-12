67b0d37b6ec15b69b3d51e3903a072f3eb6f8122
s1F10230307 <136313532+s1F10230307@users.noreply.github.com>
2024-12-13 02:56:31 +0900

    ログイン機能の追加(※元々エラー表示だったため動作確認できずにコミットプッシュします)

--- team2app/views.py
+++ team2app/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 def index(request):
 	return render(request, 'index.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')  # ログイン成功後にリダイレクト
+        else:
+            return render(request, 'team2app/login.html', {'error': 'ユーザー名またはパスワードが間違っています。'})
+    return render(request, 'team2app/login.html')
