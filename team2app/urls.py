b5b2f83c7e61c4d0d65596795ea88f7bcafcb675
Shunsuke <s1f102402079@iniad.org>
2024-12-12 20:03:07 +0900

    コメント投稿機能

--- team2app/urls.py
+++ team2app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('', views.post_list, name='post_list'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
--- team2app/views.py
+++ team2app/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+
 
 def index(request):
 	return render(request, 'index.html')
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'team2app/post_list.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        # コメントを作成する処理
+        text = request.POST.get('text')
+        if text:
+            Comment.objects.create(post=post, text=text)
+            return redirect('post_detail', post_id=post_id)
+    return render(request, 'team2app/post_detail.html', {'post': post})
