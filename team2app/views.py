fabdd66444f33da78e95923bfa866dcdae382fa0
s1f10240292 <s1f102402923@iniad.org>
2024-12-13 14:30:23 +0900

    エラー修正

--- team2app/views.py
+++ team2app/views.py
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
 
-=======
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
->>>>>>> 32ebcd5a2fd550772b2b0ed982a1c06e1cf50899
 
 def index(request):
 	return render(request, 'team2app/index.html')
 
-<<<<<<< HEAD
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'team2app/post_list.html', {'posts': posts})
@@ -24,16 +18,3 @@ def post_detail(request, post_id):
             Comment.objects.create(post=post, text=text)
             return redirect('post_detail', post_id=post_id)
     return render(request, 'team2app/post_detail.html', {'post': post})
-=======
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('index')  # ログイン成功後にリダイレクト
-        else:
-            return render(request, 'team2app/login.html', {'error': 'ユーザー名またはパスワードが間違っています。'})
-    return render(request, 'team2app/login.html')
->>>>>>> 32ebcd5a2fd550772b2b0ed982a1c06e1cf50899
